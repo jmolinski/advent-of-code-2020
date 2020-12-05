8724d0f19151def4a1d25099d30b27213eacc091
Jakub Molinski <jakub@molinski.dev>
2020-12-05 11:55:52 +0100

    Day 5 [python]

--- day5/day5.py
+++ day5/day5.py
@@ -1,11 +1,10 @@
 with open("input.txt") as f:
-    bin_loc = [
+    bin_seats = {
         l.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1")
         for l in f.readlines()
-    ]
+    }
 
-seat_ids = {int(loc[:7], 2) * 8 + int(loc[7:], 2) for loc in bin_loc}
-
+seat_ids = {int(s, 2) for s in bin_seats}
 my_seat = [
     s
     for s in set(range(1, max(seat_ids))) - seat_ids
